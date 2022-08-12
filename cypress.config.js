32b6e96d65c1f0eba1737253a0bec317d107758a
gabrielcmoris <gabrielcmoris@gmail.com>
2022-08-12 10:49:32 +0200

    excude all preset tests

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    excludeSpecPattern: "**/2-advanced-examples/*",
-    //excludes tests of the specified path
+    excludeSpecPattern: ["**/2-advanced-examples/*", "**/1-getting-started/*"],
+    //excludes tests of the specified path can be string or array
   },
 });
