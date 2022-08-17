44d0028faebd4f27c2634209a9b8a2848ce22636
gabrielcmoris <gabrielcmoris@gmail.com>
2022-08-17 15:33:15 +0200

    more test cases

--- cypress/e2e/learning/03nextjs-mongodb-app.cy.js
+++ cypress/e2e/learning/03nextjs-mongodb-app.cy.js
@@ -1,43 +1,47 @@
 /// <reference types="cypress" />
 
 describe("test feeds ae available", () => {
-  it("visit feed page", () => {
-    cy.visit("http://localhost:3000/feed");
+  let baseUrl;
+  before(() => {
+    baseUrl = "http://localhost:3000";
   });
-  it("counts the feed", () => {
+
+  beforeEach(() => {
     //I Mock the data
-    cy.intercept("get", "http://localhost:3000/api/posts?limit=10", {
+    cy.intercept("get", `${baseUrl}/api/posts?limit=10`, {
       fixture: "03nextjs-mongodb-app-mock-data.json",
     });
+  });
+
+  it("visit feed page", () => {
+    cy.visit(`${baseUrl}/feed`);
+  });
+  it("counts the feed", () => {
     cy.get(".PostList_root__Cj_24")
       .find(".Post_root__6WEkA")
       .should("be.visible")
       .and("have.length", 2);
   });
   it("sign in a user", () => {
-    cy.visit("http://localhost:3000/login");
+    cy.visit(`${baseUrl}/login`);
     cy.get("input[type=email]").type("mock@mail.com");
     cy.get("input[type=password]").type("password");
     cy.get("form").submit();
   });
 
   it("create a post", () => {
-    //1. check that we have only 2 posts
-    cy.intercept("get", "http://localhost:3000/api/posts?limit=10", {
-      fixture: "03nextjs-mongodb-app-mock-data.json",
-    });
     cy.get(".PostList_root__Cj_24")
       .find(".Post_root__6WEkA")
       .should("be.visible")
       .and("have.length", 2);
     //2. create a post
-    cy.intercept("post", "http://localhost:3000/api/posts", {
+    cy.intercept("post", `${baseUrl}/api/posts`, {
       body: { content: "I am just writting whatever" },
     });
 
     //this wont be sent to the server, it is just mocked
     cy.get(".Input_input__fo8G3")
-      .type("wtting form cypress")
+      .type("writting form cypress")
       .then(() => {
         cy.get("form").submit();
         cy.contains("You have posted successfully");
